f088b55eb7289dbc5a3b619956170994e17ba831
Anders Tornøe <ato@kvuc.dk>
2016-08-15 12:00:16 +0200

    init commit

---
+++ infographics.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".svg_bgr").slideToggle(0, function() {
+        console.log("anim complete");
+    });
+
+    $(".svg_container").click(function() {
+        $(".svg_bgr").slideToggle(2000, function() {
+            console.log("anim complete");
+        });
+    });
+});
