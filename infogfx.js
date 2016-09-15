7f1f523577e84b4b82ef8dc8926abc87d7ce061a
Anders Tornøe <ato@kvuc.dk>
2016-09-15 14:21:09 +0200

    gui, pan ved zoomed in, div fades

--- infogfx.js
+++ infogfx.js
@@ -1,14 +1,21 @@
     //var xpos = -Math.abs(($(".svgImage").width() - $(".svg_landscape").width()) / 2);
     //$(".svgImage").css("left", xpos)
-    
+
+    var zoomed = false;
+    $(".zoomedIn_container").fadeOut(0);
+    var active_tekst; 
+
+
+
 
     $('.btn_info_gfx').on('click touchstart', function(e) {
+        active_tekst = $(this).text();
         if (e.type == 'click') {
             console.log('Mouse Click');
             zoomIn(e);
             console.log('Mouse: ' + touch);
         } else {
-            
+
             var touch = e.originalEvent.touches[0];
             zoomIn(touch);
             console.log('Touch: ' + touch);
@@ -18,10 +25,23 @@
 
     $(".btn-back").fadeOut(0);
 
-    
+    $('.btn').button();
+
+
 
     var $panzoom = $(".svg_landscape").panzoom({
-        disablePan: false,
+
+        panOnlyWhenZoomed: true,
+        onPan: function() {
+
+            if (zoomed ==false){
+                 $panzoom.panzoom("reset");
+            }
+
+
+            console.log("Zoomed: " + zoomed);
+
+        },
 
     });
 
@@ -34,6 +54,13 @@
             focal: e
         });
         $(".btn-back").fadeIn(1000).click(zoomOut);
+        zoomed = true;
+
+        $(".zoomedIn_container").fadeIn();
+        $(".zoomedTitle").html("Zoom på: " + active_tekst);
+
+        $(".gui_container").fadeOut();
+        $(".btn_info_gfx").fadeOut();
     };
 
     function zoomOut(e) {
@@ -45,9 +72,13 @@
             increment: 1,
             animate: true
         });*/
+        zoomed = false;
+         $(".zoomedIn_container").fadeOut();
+        $(".gui_container").fadeIn();
+        $(".btn_info_gfx").fadeIn();
     };
 
-    $(".svg_landscape").css("-webkit-user-select","none");
+    $(".svg_landscape").css("-webkit-user-select", "none");
 
 
 
