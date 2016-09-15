432dba10db2784e9ebdff0d0f4cc810a3dc9556a
Anders Tornøe <ato@kvuc.dk>
2016-09-15 09:28:10 +0200

    Lidt mere realistiske % positioner. Zoom out position absolute. mm

--- infogfx.js
+++ infogfx.js
@@ -1,24 +1,43 @@
     //var xpos = -Math.abs(($(".svgImage").width() - $(".svg_landscape").width()) / 2);
     //$(".svgImage").css("left", xpos)
-    $(".btn_info_gfx").click(infoGfx)
+    
 
-    $(".back").click(zoomback);
+    $('.btn_info_gfx').on('click touchstart', function(e) {
+        if (e.type == 'click') {
+            console.log('Mouse Click');
+            zoomIn(e);
+            console.log('Mouse: ' + touch);
+        } else {
+            
+            var touch = e.originalEvent.touches[0];
+            zoomIn(touch);
+            console.log('Touch: ' + touch);
+        }
+
+    });
+
+    $(".btn-back").fadeOut(0);
+
+    
 
     var $panzoom = $(".svg_landscape").panzoom({
-        contain: false
+        disablePan: false,
 
     });
 
 
-    function infoGfx(e) {
+    function zoomIn(e) {
+        //
         $panzoom.panzoom('zoom', false, {
-            increment: 2,
+            increment: 2.5,
             animate: true,
             focal: e
         });
+        $(".btn-back").fadeIn(1000).click(zoomOut);
     };
 
-    function zoomback(e) {
+    function zoomOut(e) {
+        $(".btn-back").hide();
         $panzoom.panzoom("reset");
 
         /*console.log($panzoom.css("margin-left"));
@@ -27,6 +46,8 @@
             animate: true
         });*/
     };
+
+    $(".svg_landscape").css("-webkit-user-select","none");
 
 
 
